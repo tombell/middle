256fa9be568a51f6dfc55b25297b84f3826e9443
Tom Bell <tomb@tomb.io>
2024-02-11 23:09:24 +0000

    Add recovery middleware

---
+++ ware/recovery_test.go
@@ -0,0 +1,39 @@
+package ware_test
+
+import (
+	"bytes"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/matryer/is"
+
+	"github.com/tombell/middle"
+	"github.com/tombell/middle/ware"
+)
+
+func TestRecovery(t *testing.T) {
+	is := is.New(t)
+
+	buf := bytes.NewBufferString("")
+	logger := slog.New(slog.NewTextHandler(buf, nil))
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		panic("boom town")
+	})
+
+	fn := middle.Use(
+		ware.Recovery(),
+		ware.Logger(logger),
+	)(handler)
+
+	req := httptest.NewRequest("GET", "https://example.com/", nil)
+	resp := httptest.NewRecorder()
+
+	fn.ServeHTTP(resp, req)
+
+	is.Equal(resp.Code, http.StatusInternalServerError)
+	is.True(strings.Contains(buf.String(), `level=ERROR msg="recovered from panic" err="boom town"`+"\n"))
+}
