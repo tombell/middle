94dd7334733c7d1eb77f0cac449f25ccdf6c118f
Tom Bell <tomb@tomb.io>
2024-02-11 23:02:18 +0000

    Add request logging middleware

---
+++ ware/request_logging_test.go
@@ -0,0 +1,40 @@
+package ware_test
+
+import (
+	"bytes"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/matryer/is"
+
+	"github.com/tombell/middle"
+	"github.com/tombell/middle/ware"
+)
+
+func TestRequestLogging(t *testing.T) {
+	is := is.New(t)
+
+	buf := bytes.NewBufferString("")
+	logger := slog.New(slog.NewTextHandler(buf, nil))
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("created"))
+	})
+
+	fn := middle.Use(
+		ware.RequestLogging(),
+		ware.Logger(logger),
+	)(handler)
+
+	req := httptest.NewRequest("GET", "https://example.com/", nil)
+	resp := httptest.NewRecorder()
+
+	fn.ServeHTTP(resp, req)
+
+	is.True(strings.Contains(buf.String(), `level=INFO msg=http:started method=GET path=/`+"\n"))
+	is.True(strings.Contains(buf.String(), `level=INFO msg=http:finished method=GET path=/ status=201 size=7 elapsed=`))
+}
