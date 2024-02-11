d5de1f27db9045d40333721b6ebcf96ecd051e4f
Tom Bell <tomb@tomb.io>
2024-02-11 22:18:31 +0000

    Add Use function to the middle package

---
+++ middle_test.go
@@ -0,0 +1,38 @@
+package middle_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/matryer/is"
+
+	"github.com/tombell/middle"
+)
+
+func TestMiddleUse(t *testing.T) {
+	result := ""
+
+	handle := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		result += "handler"
+	})
+
+	mw1 := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			result += "one"
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	mw2 := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			result += "two"
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	fn := middle.Use(mw1, mw2)
+	fn(handle).ServeHTTP(nil, nil)
+
+	is := is.New(t)
+	is.Equal("twoonehandler", result) // expect to be the same
+}
